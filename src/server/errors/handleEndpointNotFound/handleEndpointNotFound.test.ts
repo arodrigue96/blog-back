60a305d74b5a1fbe277f786141a119cf35cf95cb
Aniol Rodriguez <arodrigue96@gmail.com>
2024-11-21 11:27:40 +0100

    Change error to ServerError

--- src/server/errors/handleEndpointNotFound/handleEndpointNotFound.test.ts
+++ src/server/errors/handleEndpointNotFound/handleEndpointNotFound.test.ts
@@ -1,14 +1,15 @@
 import { type Response, type Request } from "express";
 import handleEndpointNotFound from "./handleEndpointNotFound";
+import ServerError from "../ServerError/ServerError";
 
 describe("Given the handleEndpointNotFound middleware", () => {
   describe("When it receives next function", () => {
     test("Then it should call next function with a 'Endpoint not found' 404 error", () => {
+      const statusCode = 404;
+      const errorMessage = "Endpoint not found";
       const next = jest.fn();
-      const expectedError = {
-        statusCode: 404,
-        message: "Endpoint not found",
-      };
+
+      const expectedError = new ServerError(errorMessage, statusCode);
 
       handleEndpointNotFound({} as Request, {} as Response, next);
 
--- src/server/errors/handleEndpointNotFound/handleEndpointNotFound.ts
+++ src/server/errors/handleEndpointNotFound/handleEndpointNotFound.ts
@@ -1,11 +1,12 @@
 import { type Response, type Request, type NextFunction } from "express";
+import ServerError from "../ServerError/ServerError.js";
 
 const handleEndpointNotFound = (
   _req: Request,
   _res: Response,
   next: NextFunction,
 ) => {
-  const error = { message: "Endpoint not found", statusCode: 404 };
+  const error = new ServerError("Endpoint not found", 404);
 
   next(error);
 };
