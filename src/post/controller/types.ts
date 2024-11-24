94baad9f2a5d749bbab01180bccd9982f7150954
Aniol Rodriguez <arodrigue96@gmail.com>
2024-11-24 22:44:38 +0100

    Create deletePostWithId method of the PostController class

--- src/post/controller/types.ts
+++ src/post/controller/types.ts
@@ -3,4 +3,5 @@ import { type Request, type Response } from "express";
 export interface PostsControllerStructure {
   getPosts: (_req: Request, res: Response) => void;
   createPost: (req: Request, res: Response) => void;
+  deletePostWithId: (req: Request, res: Response) => void;
 }
